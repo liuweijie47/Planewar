3ac1014f3646c0ec3877c55f01a7ea9d92595a66
liuweijie47 <68490424+liuweijie47@users.noreply.github.com>
2020-07-19 12:43:37 +0800

    Add files via upload

---
+++ wuqi.h
@@ -0,0 +1,20 @@
+﻿#ifndef WUQI_H
+#define WUQI_H
+#include<QPixmap>
+#include<QRect>
+
+class wuqi
+{
+public:
+    wuqi();
+   void updatePosition();
+   public:
+    QPixmap m_wuqi;
+    int m_x;
+    int m_y;
+    QRect m_Rect;
+    bool m_free;
+    int m_speed;
+};
+
+#endif // WUQI_H
